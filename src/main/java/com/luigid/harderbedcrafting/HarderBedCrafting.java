3efb159178945ae9082f32150a941e3690a7cf64
LuigiD200 <luigidluigid@gmail.com>
2024-09-07 14:22:15 +0200

    IDK what I'm doing. Mental Crisis #1

--- src/main/java/com/luigid/harderbedcrafting/HarderBedCrafting.java
+++ src/main/java/com/luigid/harderbedcrafting/HarderBedCrafting.java
@@ -1,22 +1,25 @@
 package com.luigid.harderbedcrafting;
 
 import com.luigid.harderbedcrafting.proxy.CommonProxy;
-import com.luigid.harderbedcrafting.util.Reference;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+@Mod(modid = HarderBedCrafting.MOD_ID, name = HarderBedCrafting.NAME, version = HarderBedCrafting.VERSION)
 public class HarderBedCrafting {
-
+    public static final String MOD_ID = "harderbedcrafting";
+    public static final String NAME = "Harder Bed Crafting";
+    public static final String VERSION = "1.O";
+    private static final String CLIENT = "com.luigid.harderbedcrafting.proxy.ClientProxy";
+    private static final String COMMON = "com.luigid.harderbedcrafting.proxy.CommonProxy";
 
     @Mod.Instance
     public static HarderBedCrafting instance;
 
 
-    @SidedProxy(clientSide = Reference.CLIENT, serverSide = Reference.COMMON)
+    @SidedProxy(clientSide = CLIENT, serverSide = COMMON)
     public static CommonProxy proxy;
 
 
