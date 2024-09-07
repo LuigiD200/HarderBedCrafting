5e30a049573e4b1d2e42f8ce5d134c0efcd99a4a
LuigiD200 <luigidluigid@gmail.com>
2024-09-07 12:48:49 +0200

    Created the main class and set up relative components

---
+++ src/main/java/com/luigid/harderbedcrafting/HarderBedCrafting.java
@@ -0,0 +1,34 @@
+package com.luigid.harderbedcrafting;
+
+import com.luigid.harderbedcrafting.proxy.CommonProxy;
+import com.luigid.harderbedcrafting.util.Reference;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+public class HarderBedCrafting {
+
+
+    @Mod.Instance
+    public static HarderBedCrafting instance;
+
+
+    @SidedProxy(clientSide = Reference.CLIENT, serverSide = Reference.COMMON)
+    public static CommonProxy proxy;
+
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event) {}
+
+    @Mod.EventHandler
+    public static void init(FMLInitializationEvent event) {}
+
+    @Mod.EventHandler
+    public static void postInit(FMLPostInitializationEvent event) {}
+
+
+
+}
---
+++ src/main/java/com/luigid/harderbedcrafting/util/Reference.java
@@ -0,0 +1,11 @@
+package com.luigid.harderbedcrafting.util;
+
+public class Reference {
+    public static final String MOD_ID = "harderbedcrafting";
+    public static final String NAME = "Harder Bed Crafting";
+    public static final String VERSION = "1.O";
+
+
+    public static final String CLIENT = "com.luigid.harderbedcrafting.proxy.ClientProxy";
+    public static final String COMMON = "com.luigid.harderbedcrafting.proxy.CommonProxy";
+}
