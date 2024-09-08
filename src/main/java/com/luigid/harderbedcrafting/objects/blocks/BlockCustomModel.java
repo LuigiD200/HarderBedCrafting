02618017215038870c0b7f9b2e2fe55d11653150
LuigiD200 <luigidluigid@gmail.com>
2024-09-08 17:25:47 +0200

    Updated the BoundingBox of the Custom Model Block

--- src/main/java/com/luigid/harderbedcrafting/objects/blocks/BlockCustomModel.java
+++ src/main/java/com/luigid/harderbedcrafting/objects/blocks/BlockCustomModel.java
@@ -7,23 +7,28 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 
 public class BlockCustomModel extends BlockBase {
-    public static final AxisAlignedBB CUSTOM_MODEL_AB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D);
+    public static final AxisAlignedBB PART1 = new AxisAlignedBB(0.25D, 0.0D, 0.375D, 0.75D, 0.5D, 0.625D);
+    public static final AxisAlignedBB PART2 = new AxisAlignedBB(0.5625D, 0.5D, 0.4375D, 0.6875D, 0.6875D, 0.5625D);
+    public static final AxisAlignedBB PART3 = new AxisAlignedBB(0.3125D, 0.5D, 0.4375D, 0.4375D, 0.6875D, 0.5625D);
+
+    public static final AxisAlignedBB COMBINED_BOUNDING_BOX = PART1.union(PART2).union(PART3);
+
     public BlockCustomModel(String name, Material material) {
         super(name, material);
     }
 
-    public boolean isFullCube(IBlockState state)
-    {
+    @Override
+    public boolean isFullCube(IBlockState state) {
         return false;
     }
 
-    public boolean isOpaqueCube(IBlockState state)
-    {
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
         return false;
     }
 
     @Override
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-        return CUSTOM_MODEL_AB;
+        return COMBINED_BOUNDING_BOX; // Return the combined bounding box for rendering
     }
 }
